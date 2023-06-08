28ec42c5195d4c1a67af91b9121a291ce47dc22c
Huseyn Huseynzade <114292288+huseynhk@users.noreply.github.com>
2023-06-08 05:11:17 +0400

    pag2

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,80 +1,3 @@
-// import React from "react";
-// import "./apiDataViewer.css";
-
-// const Pagination = ({
-//   itemsPerPage,
-//   totalItems,
-//   currentPage,
-//   onPageChange
-// }) => {
-//   const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-//   // Handle page number click
-//   const handlePageClick = (pageNumber) => {
-//     onPageChange(pageNumber);
-//   };
-
-//   const getButtonRange = () => {
-//     const range = [];
-
-//     // Calculate the minimum button number to display
-//     let minButton = currentPage - 2;
-//     if (minButton <= 0) {
-//       minButton = 1;
-//     }
-
-//     // Calculate the maximum button number to display
-//     let maxButton = minButton + 4;
-//     if (maxButton > totalPages) {
-//       maxButton = totalPages;
-//       minButton = maxButton - 4;
-//       if (minButton <= 0) {
-//         minButton = 1;
-//       }
-//     }
-
-//     for (let i = minButton; i <= maxButton; i++) {
-//       range.push(i);
-//     }
-
-//     return range;
-//   };
-
-//   return (
-//     <div className="pagination">
-//       {currentPage > 1 && (
-//         <button
-//           onClick={() => handlePageClick(1)}
-//           className="pagination-button"
-//         >
-//           First
-//         </button>
-//       )}
-
-//       {getButtonRange().map((number) => (
-//         <button
-//           key={number}
-//           onClick={() => handlePageClick(number)}
-//           className={`pagination-button ${currentPage === number ? "active" : ""}`}
-//         >
-//           {number}
-//         </button>
-//       ))}
-
-//       {currentPage < totalPages && (
-//         <button
-//           onClick={() => handlePageClick(totalPages)}
-//           className="pagination-button"
-//         >
-//           Last
-//         </button>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Pagination;
-
 import React from "react";
 import "./apiDataViewer.css";
 
@@ -110,11 +33,6 @@ const Pagination = ({
       }
     }
 
-    // Additional logic to change the first button to the respective page number
-    if (currentPage === maxButton && maxButton - 3 > 1) {
-      minButton = maxButton - 3;
-    }
-
     for (let i = minButton; i <= maxButton; i++) {
       range.push(i);
     }
@@ -126,10 +44,10 @@ const Pagination = ({
     <div className="pagination">
       {currentPage > 3 && (
         <button
-          onClick={() => handlePageClick(currentPage > 4 ? 1 : "First")}
+          onClick={() => handlePageClick(1)}
           className="pagination-button"
         >
-          {currentPage > 3 ? "First" : 1}
+          First
         </button>
       )}
 
